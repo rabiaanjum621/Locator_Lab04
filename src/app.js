382c05011026f0d7efbf645f6187901fd3c3a29d
NiteshGoyal <nitesh.goyal@gdn-commerce.com>
2022-01-27 07:56:00 +0530

    added null checks and removed alert

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
-    alert("Hello");
-    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
-    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
-    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-    document.getElementById("shareTarget").innerHTML = 'Title shared: ' + parsedUrl.searchParams.get('title')
-    + 'Text shared: ' + parsedUrl.searchParams.get('text') + 'URL shared: ' + parsedUrl.searchParams.get('url');
+    // alert("Hello");
+    // console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    // console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    // console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+    var title = parsedUrl.searchParams.get('title');
+    var text = parsedUrl.searchParams.get('text');
+    var url = parsedUrl.searchParams.get('url');
+    var result = "";
+    if(title != null) {
+      result += title
+    }
+    if(text != null) {
+      result += " " + text
+    }
+    if(url != null) {
+      result += " " + url 
+    }
+    document.getElementById("shareTarget").innerHTML = result;
   });
 
 
