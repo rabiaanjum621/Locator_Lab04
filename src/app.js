c99836c02a8af7489524d635846be5be84d8beef
Rabia <rabia@Rabias-MacBook-Air.local>
2022-04-05 01:06:40 -0400

    lab 04  commit

--- src/app.js
+++ src/app.js
@@ -1,118 +1,122 @@
-const navigator = document.querySelector("#navigator");
-window.addEventListener("DOMContentLoaded", () => {});
-
+var map;
+var markers = [];
+var markersInfo = [];
+let accessToken =
+  "pk.eyJ1IjoicmFiaWE2MjEiLCJhIjoiY2wxa2NyNXFvMHplajNlcGJpN2RndDE1YyJ9.JPZwuNdbIXFa8Ct94EmZ7w";
 document.addEventListener("init", (e) => {
   if (e.target.id === "home") {
-    fetchListPageData();
-  } else if (e.target.id === "details") {
-    fetchDetailsPageData(navigator.topPage.data.pageId);
+    map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
+        accessToken,
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: accessToken,
+      }
+    ).addTo(map);
+    map.on("click", onMapClick);
   }
+  var saveBtn = document.querySelector("#saveAll")
+  saveBtn.addEventListener('click', (e) =>{
+    console.log("save button clicked")
+    saveMarkersState();
+  })
+  reLoadMarkers();
 });
 
-const fetchListPageData = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
-    .then((response) => response.json())
-    .then((data) => addDataToList(data));
+
+const saveMarkersState = async () => {
+try {
+    await localforage.setItem("state", markersInfo);
+  } catch (e) {
+    return console.log("error", e);
+  }
+  console.log("successfully saved");
 };
 
-const addDataToList = (data) => {
-  var list = document.querySelector("#pokemonList");
-  data.results.forEach((element, index) => {
-    list.appendChild(createChevronListItem(element.name, index));
+const reLoadMarkers = async () => {
+  try {
+    const newState = await localforage.getItem("state");
+    if (newState && Object.keys(newState).length !== 0) {
+      markersInfo = [...newState];
+    }
+    markersInfo.forEach((e) => {
+      var marker = L.marker([e.lat, e.lng]).addTo(map);
+      marker
+      .bindPopup(
+        e.message +
+          ` <br><br><button onClick="clearMarker('${e.lat}', '${e.lng}')" >clear</button>`
+      )
+      .openPopup();
+    L.popup().setLatLng([e.lat, e.lng]).setContent(e.message);
+      markers.push(marker);
+    });
+  } catch (e) {
+    console.log("error loading state", e);
+  }
+};
+
+const onMapClick = function (e) {
+  console.log("onMapClick");
+  var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+  ons.notification.prompt("Prompt!").then(function (input) {
+    var message = input ? input : "I am a popup!";
+    marker
+      .bindPopup(
+        message +
+          ` <br><br><button onClick="clearMarker('${e.latlng.lat}', '${e.latlng.lng}')" >clear</button>`
+      )
+      .openPopup();
+    L.popup().setLatLng([e.latlng.lat, e.latlng.lng]).setContent(message);
+    markers.push(marker);
+
+    var markerData = {
+      lat: e.latlng.lat,
+      lng: e.latlng.lng,
+      message: message
+    };
+
+    markersInfo.push(markerData);
   });
 };
 
-const itemClickListener = (e) => {
-  changePage("details.html", { pageId: e.currentTarget.id });
-};
-
-const fetchDetailsPageData = (pageId) => {
-  fetch("https://pokeapi.co/api/v2/pokemon/" + pageId)
-    .then((response) => response.json())
-    .then((data) => bindDetailPage(data));
-};
-
-const bindDetailPage = (data) => {
-  let tbTitle = document.querySelector("#tbTitle");
-  let imgPokemon = document.querySelector("#imgPokemon");
-  let pokemonTitle = document.querySelector("#pokemonTitle");
-  
-  
-  tbTitle.innerHTML = data.species.name;
-  imgPokemon.src = data.sprites.front_default;
-  pokemonTitle.innerHTML = data.species.name + " #" + data.game_indices[0].game_index;
-  addTypesList(data.types);
-  addStatList(data.stats);
-};
-
-const addTypesList = (types) => {
-  let typesListNode = document.querySelector("#typesList");
-  types.forEach((typeItem) => {
-    typesListNode.appendChild(createListItem(typeItem.type.name));
+function removeAllMarkers() {
+  markers.forEach((e) => {
+    map.removeLayer(e);
+    markers = [];
+    markersInfo = [];
+    saveMarkersState();
   });
 }
 
-const addStatList = (stats) => {
-  let statsListNode = document.querySelector("#statsList");
-  stats.forEach((statsItem) => {
-    statsListNode.appendChild(
-        createListItem(
-          statsItem.stat.name, statsItem.effort, statsItem.base_stat
-        ));
-  });
+function clearMarker(lat, lng) {
+  let matchedMarker = markers.find(
+    (element) => element._latlng.lat == lat && element._latlng.lng == lng
+  );
+  if (matchedMarker) {
+    map.removeLayer(matchedMarker);
+    let index = markers.indexOf(matchedMarker);
+    if (index > -1) {
+      markers.splice(index, 1);
+    }
+  }
+  let markerObj = markersInfo.find(
+    (element) => element.lat == lat && element.lng == lng
+  );
+  if (markerObj) {
+    let index = markersInfo.indexOf(markerObj);
+    if (index > -1) {
+      markersInfo.splice(index, 1);
+    }
+  }
+  saveMarkersState();
 }
 
-const changePage = (page, data) => {
-  navigator.pushPage(page, { data });
-};
-
-function createChevronListItem(input, index) {
-  let item = document.createElement("ons-list-item");
-  item.setAttribute("modifier", "chevron");
-  item.setAttribute("tappable", "true");
-  item.innerHTML = input;
-  item.id = "" + (index + 1);
-  item.addEventListener("click", itemClickListener);
-  return item;
+function userCurrentLocation() {
+  map.locate({ setView: true, maxZoom: 16 });
 }
-
-function createListItem(name, effort, stat) {
-  let item = document.createElement("ons-list-item");
-  var spanItem = document.createElement("span");
-  spanItem.innerHTML = name;
-  spanItem.style.marginRight = "10px";
-  item.appendChild(spanItem);
-  
-  if(typeof effort !== "undefined") {
-  spanItem = document.createElement("span");
-  spanItem.className = "notification";
-  spanItem.style.backgroundColor = "blue";
-  spanItem.style.marginRight = "10px";
-  spanItem.innerHTML = effort;
-  item.appendChild(spanItem);
-  }
-
-  if(typeof stat !== "undefined") {
-  spanItem = document.createElement("span");
-  spanItem.className = "notification";
-  spanItem.style.backgroundColor = "red";
-  spanItem.innerHTML = stat;
-  item.appendChild(spanItem);
-  }
-  return item;
-}
-
-const popPage = () => navigator.popPage();
-// Padd the history with an extra page so that we don't exit right away
-window.addEventListener('load', () => window.history.pushState({ }, ''));
-// When the browser goes back a page, if our navigator has more than one page we pop the page and prevent the back event by adding a new page
-// Otherwise we trigger a second back event, because we padded the history we need to go back twice to exit the app.
-window.addEventListener('popstate', () => {
-  const { pages } = navigator;
-  if (pages && pages.length > 1) {
-    popPage();
-    window.history.pushState({ }, '');
-  } else {
-    window.history.back();
-  }
-});
